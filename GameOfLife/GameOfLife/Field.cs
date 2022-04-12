48db244bd499f888d59b5d9408e1515bd6e1a587
Sergejs Hramkovs <sergejs.hramkovs@accenture.com>
2022-04-12 10:55:57 +0300

    Add create, draw and seed functions

---
+++ GameOfLife/GameOfLife/Field.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Field
+    {
+        private int _fieldHeight { get; set; }
+        public int _fieldWidth { get; set; }
+
+        string[,] fieldArray;
+
+        public Field(int height, int width)
+        {
+            _fieldHeight = height;
+            _fieldWidth = width;
+        }
+
+        public void CreateField()
+        {
+            fieldArray = new string[_fieldWidth, _fieldHeight];
+
+            for (int j = 0; j < _fieldHeight; j++)
+            {
+                for (int i = 0; i < _fieldWidth; i++)
+                {
+                    fieldArray[i, j] = "-";
+                }
+            }
+        }
+
+        public void DrawField()
+        {
+            Console.WriteLine();
+
+            for (int j = 0; j < _fieldHeight; j++)
+            {
+                for (int i = 0; i < _fieldWidth; i++)
+                {
+                    Console.Write(" " + fieldArray[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void SeedField()
+        {
+            int cellX;
+            int cellY;
+            string input;
+            
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("To stop seeding enter 'stop'");
+                Console.Write("Enter X coordinate of the cell: ");
+                input = Console.ReadLine();
+
+                if (input == "stop")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("The seeding has been stopped!");
+                    break;
+                }
+
+                cellX = Convert.ToInt32(input);
+
+                Console.Write("Enter Y coordinate of the cell: ");
+                input = Console.ReadLine();
+
+                if (input == "stop")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("The seeding has been stopped!");
+                    break;
+                }
+
+                cellY = Convert.ToInt32(input);      
+
+                fieldArray[cellX - 1, cellY - 1] = "X";
+                DrawField();
+            }            
+        }
+    }
+}
---
+++ GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,15 @@
+﻿using GameOfLife;
+
+int height;
+int width;
+
+Console.Write("Enter the height of the field: ");
+height = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the height of the field: ");
+width = Convert.ToInt32(Console.ReadLine());
+
+Field field = new Field(height, width);
+field.CreateField();
+field.DrawField();
+field.SeedField();
