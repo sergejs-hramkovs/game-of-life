cc49e676e6e4e00cb1ac2563881f34124022c5a8
Sergejs Hramkovs <sergejs.hramkovs@accenture.com>
2022-05-18 09:24:18 +0300

    Remove outdated tests

--- GameOfLife/GameOfLife.Tests/InputControllerTest.cs
+++ GameOfLife/GameOfLife.Tests/InputControllerTest.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GameOfLife.Models;
-using GameOfLife.Interfaces;
-using GameOfLife.Views;
 using System;
 
 namespace GameOfLife.Tests
@@ -9,39 +6,6 @@ namespace GameOfLife.Tests
     [TestClass]
     public class InputControllerTest
     {
-        [TestMethod]
-        [DataRow(ConsoleKey.D1, 3, 3)]
-        [DataRow(ConsoleKey.D2, 5, 5)]
-        [DataRow(ConsoleKey.D3, 10, 10)]
-        [DataRow(ConsoleKey.D4, 20, 20)]
-        [DataRow(ConsoleKey.D5, 75, 40)]
-        public void TestCheckInputMainMenu_PremadeInput(ConsoleKey keyPressed, int length, int width)
-        {
-            InputController inputController = new();
-            GameFieldModel gameField = new(length, width);
-
-            //inputController.CheckInputMainMenu(keyPressed);
-
-            Assert.AreEqual(gameField.Length, length);
-            Assert.AreEqual(gameField.Width, width);
-        }
-
-        [TestMethod]
-        [DataRow(ConsoleKey.D1, 40, 30)]
-        [DataRow(ConsoleKey.D2, 37, 40)]
-        public void TestCheckGliderGunMenu_D1D2Input(ConsoleKey keyPressed, int length, int width)
-        {
-            MainEngine engine = new();
-            InputController inputController = new();
-            inputController.Inject(engine);
-            GameFieldModel gameField;
-
-            //gameField = inputController.CheckInputGliderGunMenu();
-
-            //Assert.AreEqual(gameField.Length, length);
-            //Assert.AreEqual(gameField.Width, width);
-        }
-
         [TestMethod]
         [DataRow(ConsoleKey.LeftArrow, 1000, 900)]
         [DataRow(ConsoleKey.LeftArrow, 100, 90)]
@@ -56,7 +20,7 @@ namespace GameOfLife.Tests
             inputController.Inject(engine);
             engine.Delay = initialDelay;
 
-            //inputController.ChangeDelay();
+            inputController.ChangeDelay(keyPressed);
 
             Assert.AreEqual(newDelay, engine.Delay);
         }
